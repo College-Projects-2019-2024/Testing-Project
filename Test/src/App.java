ebb89326feb0a7baa514689058ca065462fcc935
mohamed ahmed samir <105310173+Mohamed-samir-504@users.noreply.github.com>
2023-03-16 19:49:01 +0200

    added code logic

--- Test/src/App.java
+++ Test/src/App.java
@@ -1,38 +1,11 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+
 
 public class App {
     public static void main(String[] args) {
+        Subject subb = new Subject();
 
-        BufferedReader reader;
-        try {
-            reader = new BufferedReader(new FileReader("Test\\src\\Sample.txt"));
-            String line = "x";
-
-            for (int i = 0; line != null; i++) {
-                line = reader.readLine();
-
-                if (i == 0) {
-
-                        continue;
-                } 
-                else {
-                    if (line != null) {
-                        Student current = new Student(line);
-                        System.out.println(current.getName() + '\t' + current.getStudentNumber() + '\t'
-                                + current.calculateGPA() + '\t' + current.calculateGrade() + '\n');
-                    }
-
-                }
-
-            }
-
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        subb.readFile("Test\\src\\Sample.txt");
+        subb.writeFile(subb);
     }
 
 }
--- Test/src/Subject.java
+++ Test/src/Subject.java
@@ -1,12 +1,78 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 
 public class Subject {
     String name;
     String code;
     int fullMark;
-    Student[] student;
+    ArrayList<Student>students;
     
 
+    public void readFile(String filename){
+        BufferedReader reader;
+        students = new ArrayList<Student>();
+        try {
+            reader = new BufferedReader(new FileReader(filename));
+            String line = "x";
 
+            for (int i = 0; line != null; i++) {
+                line = reader.readLine();
+
+                if (i == 0) {
+                    String[] attr = line.split(",");
+                    name = attr[0];
+                    code = attr[1];
+                    fullMark = Integer.valueOf(attr[2]);
+                } 
+                else {
+                    if (line != null) {
+                        Student current = new Student(line);
+                        students.add(current);
+                    
+                    }
+
+                }
+
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void writeFile(Subject sub){
+        FileWriter file;
+        try{
+            file = new FileWriter("output.txt");
+            BufferedWriter writebuff = new BufferedWriter(file);
+
+            writebuff.write("Subject name: " + sub.name +"\t" +"\t");
+            writebuff.write("Max mark: " + sub.fullMark);
+            writebuff.write("\n");
+            writebuff.write("\n");
+
+            for(int i = 0; i<sub.students.size(); i++){
+                writebuff.write(sub.students.get(i).getName()+"\t" );
+                writebuff.write(sub.students.get(i).getStudentNumber()+"\t" );
+                double x = sub.students.get(i).calculateGPA();
+                String s = sub.students.get(i).calculateGrade();
+                writebuff.write(x+"\t"+"\t");
+                writebuff.write(s);
+                writebuff.write("\n");
+
+            }
+
+            writebuff.close();
+        }catch(IOException except){
+            except.printStackTrace();
+        }
+    }
     
 
 }
