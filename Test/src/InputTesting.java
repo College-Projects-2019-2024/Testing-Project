cdd53e3824acc0a99fb1f1bde9e73912df666b91
marcosherif <68437948+marcosherif@users.noreply.github.com>
2023-03-20 21:36:23 +0200

    edit

--- Test/src/InputTesting.java
+++ Test/src/InputTesting.java
@@ -11,7 +11,7 @@ public class InputTesting{
     @BeforeAll
     static void init(){
         s = new Subject();
-        s.readFile("C:\\Users\\mohamed\\Desktop\\tttt.txt");
+        s.readFile("D:\\College\\Computer Year 3\\Software Testing\\Project\\Sample.txt");
     }
 
     @Test
