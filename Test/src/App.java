74c49cb8d9728be5b3b0427431ad8b275059c98f
mohamed ahmed samir <105310173+Mohamed-samir-504@users.noreply.github.com>
2023-03-27 21:32:22 +0200

    ...

--- Test/src/App.java
+++ Test/src/App.java
@@ -3,7 +3,7 @@ public class App {
     public static void main(String[] args) {
         Subject subb = new Subject();
 
-        subb.readFile("Sample.txt");
+        subb.readFile("Test\\Samplle.txt");
         subb.writeFile(subb);
         
     }
