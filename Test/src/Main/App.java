578812d30038ef214b189f6615e6be645ee5d9ab
mohamed ahmed samir <105310173+Mohamed-samir-504@users.noreply.github.com>
2023-05-14 11:20:51 +0300

    .

--- Test/src/Main/App.java
+++ Test/src/Main/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) {
 
         fileIO file = new fileIO();
-
+        
         Subject sub = file.readFile("Test\\Samplle.txt");
         
         file.writeFile(sub);
