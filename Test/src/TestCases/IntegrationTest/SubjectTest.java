9b4a60f873983d17d3f4f32ac82765b2a9dc31a2
markkashraf <54954479+markkashraf@users.noreply.github.com>
2023-05-20 20:58:15 +0300

    Update SubjectTest.java

--- Test/src/TestCases/IntegrationTest/SubjectTest.java
+++ Test/src/TestCases/IntegrationTest/SubjectTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertEquals;
 
 public class SubjectTest {
 
+    //check that data is inserted correctly
     @Test
     void test1() {
 
@@ -20,30 +21,15 @@ public class SubjectTest {
     }
 
 
+// check for the subject data for correctness
     @Test
     void test2() {
-
-        String testName = "Networks";
-        String testCode = "CSE345";
-        int maxTest = 100;
-
-        Subject subject = new Subject(testName,testCode,maxTest);
-        assertEquals(subject.getName(),testName, subject.getName());
-        assertEquals(subject.getCode(),testCode, subject.getCode());
-        assertEquals(subject.getFullMark(),maxTest, subject.getFullMark());
-    }
-
-
-
-    @Test
-    void test3() {
 
         String testName = "Graphs";
         String testCode = "CSE666";
         int maxTest = 100;
 
         Subject subject = new Subject(testName,testCode,maxTest);
-
         assertEquals(subject.checkSubjectData(),"",subject.checkSubjectData());
         
     }
