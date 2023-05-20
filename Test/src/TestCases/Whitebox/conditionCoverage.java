1e610706cd95174f881de93361efaf437be9d11a
hossamadel7 <57440281+hossamadel7@users.noreply.github.com>
2023-05-20 22:27:19 +0300

    Finish conditionCoverage.java

--- Test/src/TestCases/Whitebox/conditionCoverage.java
+++ Test/src/TestCases/Whitebox/conditionCoverage.java
@@ -8,9 +8,67 @@ public class conditionCoverage {
     
     @Test
     void test1() {
-        boolean expected = true;
-        boolean actual = Subject.checkSubjectCode("CSE123") != Subject.SUBJECT_CODE_INVALID_LENGTH;
+        boolean expected = false;
+        boolean actual = Subject.checkSubjectCode("CSE123") == Subject.SUBJECT_CODE_INVALID_LENGTH;
         assertEquals("Erroor in test 1", expected, actual);
+    }
+
+    @Test
+    void test2() {
+        boolean expected = false;
+        boolean actual = Subject.checkSubjectCode("CSE123s") == Subject.SUBJECT_CODE_INVALID_LENGTH;
+        assertEquals("Erroor in test 1", expected, actual);
+    }
+
+    @Test
+    void test3() {
+        boolean expected = true;
+        boolean actual = Subject.checkSubjectCode("CSE11") == Subject.SUBJECT_CODE_INVALID_LENGTH;
+        assertEquals("Erroor in test 1", expected, actual);
+    }
+
+    @Test
+    void test5() {
+        boolean expected = false;
+        boolean actual = Subject.checkSubjectCode("CSE123") == Subject.SUBJECT_CODE_HAS_NUMBERS;
+        assertEquals("Erroor in test 1", expected, actual);
+    }
+    @Test
+    void test6() {
+        boolean expected = true;
+        boolean actual = Subject.checkSubjectCode("1CS223") == Subject.SUBJECT_CODE_HAS_NUMBERS;
+        assertEquals("Erroor in test 1", expected, actual);
+    }
+    @Test
+    void test7() {
+        boolean expected = false;
+        boolean actual = Subject.checkSubjectCode("CSE123") == Subject.SUBJECT_CODE_HAS_LETTERS;
+        assertEquals("Erroor in test 1", expected, actual);
+    }
+    @Test
+    void test8() {
+        boolean expected = true;
+        boolean actual = Subject.checkSubjectCode("CSEabc") == Subject.SUBJECT_CODE_HAS_LETTERS;
+        assertEquals("Erroor in test 1", expected, actual);
+    }
+    @Test
+    void test9() {
+        boolean expected = false;
+        boolean actual = Subject.checkSubjectCode("CSE123") == Subject.SUBJECT_CODE_END;
+        assertEquals("Erroor in test 1", expected, actual);
+    }
+    @Test
+    void test10() {
+        boolean expected = false;
+        boolean actual = Subject.checkSubjectCode("CSE123s") == Subject.SUBJECT_CODE_END;
+        assertEquals("Erroor in test 1", expected, actual);
+    }
+    @Test
+    void test11() {
+        boolean expected = true;
+        boolean actual = Subject.checkSubjectCode("CSE123a") == Subject.SUBJECT_CODE_END;
+        assertEquals("Erroor in test 1", expected, actual);
+        
     }
 
 
